a5dff9e6f9a13c9f178353ca4d0572d309e2d10e
Francisco Montoya <franciscomontoyacasual@gmail.com>
2022-05-26 01:48:40 -0500

    Solucion de manejo de rutas

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-include "test/db.php";
-include "utilities/cookie.php";
+include __DIR__."/test/db.php";
+include __DIR__."/utilities/cookie.php";
 
 $request = $_SERVER['REQUEST_URI'];
 
@@ -16,9 +16,9 @@ switch($request){
 function set_basic_configuration(){
   $db = DB::connect_db();
   if($_COOKIE['uid'] && $_COOKIE['rid'] == 1)
-  include "views/home.php";
+  include __DIR__."/views/home.php";
   else if($_COOKIE['uid'] && ($_COOKIE['rid'] == 2 || $_COOKIE['rid'] == 4))
-  include "views/home_area_manager.php";
+  include __DIR__."/views/home_area_manager.php";
   else{
   if(isset($_POST['user_name']) && isset($_POST['password'])){
     $user_name = $_POST['user_name'];
@@ -27,7 +27,7 @@ function set_basic_configuration(){
     $result = DB::auth_user($db, $user_name, $pass);
 
     if($result['msg'])
-      include "views/login.php";
+      include __DIR__."/views/login.php";
     else if($result['rol'] == 1){
       Cookie::set_cookie('uid', $result['uid'], 4102466399);
       Cookie::set_cookie('rid', $result['rol'], 4102466399);
@@ -39,7 +39,7 @@ function set_basic_configuration(){
       header('Location: /');
     }
   }else
-    include "views/login.php";
+    include __DIR__."/views/login.php";
   }
 }
 ?>
